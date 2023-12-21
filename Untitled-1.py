0feb43f975613e868df50680e10aca1e3f920c26
Benny <benjamintp.1985@gmail.com>
2023-12-21 13:15:45 +1100

    commin one - main

---
+++ Untitled-1.py
@@ -0,0 +1,11 @@
+#  Dummy file to learn about git stash
+
+
+def my_function():
+    print("Hello World")
+
+def my_function2():
+    print("Hello World2")
+
+def my_function3():
+    print("Hello World3")
